d1d9d17c8cb59dc17ac70f7f03cd9425c53b4eb2
jiaahmad <jiaahmad9595@gmail.com>
2020-11-14 17:55:21 +0500

    Updates

---
+++ precache-manifest.b37d0e84c58c5cd49c0678d430294809.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b3c4b2f5cbb72ae1af8a212e6e8eae1",
+    "url": "/olxReplica/index.html"
+  },
+  {
+    "revision": "57a38331456062d3792b",
+    "url": "/olxReplica/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "dda76a10a8a369bc595f",
+    "url": "/olxReplica/static/css/main.e88772c1.chunk.css"
+  },
+  {
+    "revision": "57a38331456062d3792b",
+    "url": "/olxReplica/static/js/2.eea53710.chunk.js"
+  },
+  {
+    "revision": "a707de305cb89f3e32746332f99bfeef",
+    "url": "/olxReplica/static/js/2.eea53710.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dda76a10a8a369bc595f",
+    "url": "/olxReplica/static/js/main.5a426c3d.chunk.js"
+  },
+  {
+    "revision": "f0947d0519a82a74edcf",
+    "url": "/olxReplica/static/js/runtime-main.25f5bfc3.js"
+  },
+  {
+    "revision": "2f3c59a27f2cd92b5495255b9374447d",
+    "url": "/olxReplica/static/media/Capture.2f3c59a2.PNG"
+  },
+  {
+    "revision": "6dfd139f268b747009de41737bd29b63",
+    "url": "/olxReplica/static/media/bottomadd.6dfd139f.PNG"
+  },
+  {
+    "revision": "2de8b483ea4fc74ea1c2a085959d1cf5",
+    "url": "/olxReplica/static/media/homesecondadd2.2de8b483.jpg"
+  },
+  {
+    "revision": "c1e325edca9a8909b4f4a07d2404d90a",
+    "url": "/olxReplica/static/media/topadd.c1e325ed.png"
+  }
+]);
